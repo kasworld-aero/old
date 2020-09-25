f38e6085ddf1ad7f652d777c6079f0da94d087d2
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2020-09-25 17:11:48 +0530

    Update v0.01.js

--- file/javascript/v0.01.js
+++ file/javascript/v0.01.js
@@ -1,4 +1,6 @@
-document.write( '<style>\n' );
+window.onload = function(){
+  
+  document.write( '<style>\n' );
 document.write( '  #fbsb {\n' );
 document.write( '  position: fixed;\n' );
 document.write( '  bottom: -8px;\n' );
@@ -11,3 +13,6 @@ document.write( '</style>\n' );
 document.write( '<div id=\"fbsb\">\n' );
 document.write( '<a href=\"#\" onclick=\"location.href=\'https://kasworld-aero.blogspot.com/p/fb.html?furl=\'+ window.location.href;\"><img src=\"https://1.bp.blogspot.com/-TFiLkISdlHU/X2WCfgWsEAI/AAAAAAAAFUg/5-OZ7i9M5QMKBt_1-2RTYDcKegwcazl6wCLcBGAsYHQ/s320/Share%2BButton.png\" style=\"height:40px; width:40px;\"/></a>\n' );
 document.write( '  </div>' );
+  
+  
+  };
