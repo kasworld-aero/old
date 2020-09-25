4bae028529242a07844e20acfd03829fbb58f2be
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2020-09-25 16:41:12 +0530

    Create v0.01.js

---
+++ file/javascript/v0.01.js
@@ -0,0 +1,13 @@
+document.write( '<style>\n' );
+document.write( '  #fbsb {\n' );
+document.write( '  position: fixed;\n' );
+document.write( '  bottom: -8px;\n' );
+document.write( '  right: 0px;\n' );
+document.write( 'height:50px; width:70px;\n' );
+document.write( '  z-index: 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999;\n' );
+document.write( '}\n' );
+document.write( '\n' );
+document.write( '</style>\n' );
+document.write( '<div id=\"fbsb\">\n' );
+document.write( '<a href=\"#\" onclick=\"location.href=\'https://kasworld-aero.blogspot.com/p/fb.html?furl=\'+ window.location.href;\"><img src=\"https://1.bp.blogspot.com/-TFiLkISdlHU/X2WCfgWsEAI/AAAAAAAAFUg/5-OZ7i9M5QMKBt_1-2RTYDcKegwcazl6wCLcBGAsYHQ/s320/Share%2BButton.png\" style=\"height:40px; width:40px;\"/></a>\n' );
+document.write( '  </div>' );
