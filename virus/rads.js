0e651cb3f12ec9e64ef7fe7addc4b4fbcec1f728
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-05-21 20:14:14 +0530

    Update rads.js

--- virus/rads.js
+++ virus/rads.js
@@ -1,3 +1,7 @@
-document.write('<script src="https://kasworld-aero.github.io/old/virus/rads/fontawesome.js"></script> <link rel="stylesheet" type="text/css" href="https://kasworld-aero.github.io/old/virus/rads/new-min.css">');
-document.write('<div id="kasads"></div>')
+document.write('<link rel="stylesheet" type="text/css" href="https://kasworld-aero.github.io/old/virus/rads/new-min.css">');
+
+        var script = document.createElement('script'); 
+        script.src = "https://kasworld-aero.github.io/old/virus/rads/fontawesome.js";
+        document.head.appendChild(script);
+document.write('<div id="kasads"></div>');
 
