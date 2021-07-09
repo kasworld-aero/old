01b56a460f84a5f0f2956882f1e614985be32a75
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-07-09 15:30:40 +0530

    Update travel-v-9.0.2.js

--- file/javascript/new-cdn/travel-v-9.0.2.js
+++ file/javascript/new-cdn/travel-v-9.0.2.js
@@ -1,7 +1,73 @@
-var ticket1 ='<scr'+'ipt charset="utf-8"type="text/javas'+'cript">window.TP_FORM_SETTINGS=window.TP_FORM_SETTINGS||{};window.TP_FORM_SETTINGS["33849f46cca08a270db4ddb79e2354b8"]={"handle":"33849f46cca08a270db4ddb79e2354b8","widget_name":"FlightBooking","border_radius":"3","additional_marker":null,"width":null,"show_logo":false,"show_hotels":false,"form_type":"avia_hotel","locale":"en","currency":"inr","sizes":"default","search_target":"_self","active_tab":"avia","search_host":"travel.kasworld-aero.ml/flights","hotels_host":"travel.kasworld-aero.ml/hotels","hotel":"","hotel_alt":"Hotellookhelpsyoufindthebestaccomodationpricesonline.","avia_alt":"","retargeting":true,"trip_class":"economy","depart_date":null,"return_date":null,"check_in_date":null,"check_out_date":null,"no_track":false,"powered_by":false,"source_id":null,"id":244014,"marker":172519,"origin":{"name":""},"destination":{"name":""},"color_scheme":{"name":"white_blue","icons":"icons_blue","background":"#ffffff","color":"#000000","border_color":"#c1c1c1","button":"#1b9ed9","button_text_color":"#ffffff","input_border":"#ffffff"},"hotels_type":"hotellook_host","best_offer":{"locale":"en","currency":"inr","marker":172519,"search_host":"travel.kasworld-aero.ml/flights","offers_switch":false,"api_url":"//minprices-jetradar.aviasales.ru/minimal_prices/offers.json","routes":[]},"hotel_logo_host":null,"search_logo_host":"jetradar.com","hotel_marker_format":null,"hotelscombined_marker":null,"sources":[],"responsive":true,"height":460};</scr'+'ipt>';
 
-
-var ticket2 =;
-
-document.write(ticket1);
 document.write('<script charset="utf-8"src="//www.travelpayouts.com/widgets/33849f46cca08a270db4ddb79e2354b8.js?v=2135"async></scr'+'ipt>');
+
+
+  $('#flight-ticket').html('<script charset="utf-8" type="text/javascript">
+    window.TP_FORM_SETTINGS = window.TP_FORM_SETTINGS || {};
+    window.TP_FORM_SETTINGS["33849f46cca08a270db4ddb79e2354b8"] = {
+	"handle": "33849f46cca08a270db4ddb79e2354b8",
+	"widget_name": "Flight Booking",
+	"border_radius": "3",
+	"additional_marker": null,
+	"width": null,
+	"show_logo": false,
+	"show_hotels": false,
+	"form_type": "avia_hotel",
+	"locale": "en",
+	"currency": "inr",
+	"sizes": "default",
+	"search_target": "_self",
+	"active_tab": "avia",
+	"search_host": "travel.kasworld-aero.ml/flights",
+	"hotels_host": "travel.kasworld-aero.ml/hotels",
+	"hotel": "",
+	"hotel_alt": "Hotellook helps you find the best accomodation prices online.",
+	"avia_alt": "",
+	"retargeting": true,
+	"trip_class": "economy",
+	"depart_date": null,
+	"return_date": null,
+	"check_in_date": null,
+	"check_out_date": null,
+	"no_track": false,
+	"powered_by": false,
+	"source_id": null,
+	"id": 244014,
+	"marker": 172519,
+	"origin": {
+		"name": ""
+	},
+	"destination": {
+		"name": ""
+	},
+	"color_scheme": {
+		"name": "white_blue",
+		"icons": "icons_blue",
+		"background": "#ffffff",
+		"color": "#000000",
+		"border_color": "#c1c1c1",
+		"button": "#1b9ed9",
+		"button_text_color": "#ffffff",
+		"input_border": "#ffffff"
+	},
+	"hotels_type": "hotellook_host",
+	"best_offer": {
+		"locale": "en",
+		"currency": "inr",
+		"marker": 172519,
+		"search_host": "travel.kasworld-aero.ml/flights",
+		"offers_switch": false,
+		"api_url": "//minprices-jetradar.aviasales.ru/minimal_prices/offers.json",
+		"routes": []
+	},
+	"hotel_logo_host": null,
+	"search_logo_host": "jetradar.com",
+	"hotel_marker_format": null,
+	"hotelscombined_marker": null,
+	"sources": [],
+	"responsive": true,
+	"height": 460
+};
+</scr'+'ipt>
+'+'<script charset="utf-8" src="//www.travelpayouts.com/widgets/33849f46cca08a270db4ddb79e2354b8.js?v=2135" async></scr'+'ipt>
+  );
