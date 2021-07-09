0b1b4013f266200e939a8f0cc172bf2d749ea130
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-07-09 14:46:42 +0530

    Create travel-v-9.0.2.js

---
+++ file/javascript/new-cdn/travel-v-9.0.2.js
@@ -0,0 +1,72 @@
+ window.TP_FORM_SETTINGS = window.TP_FORM_SETTINGS || {};
+    window.TP_FORM_SETTINGS["33849f46cca08a270db4ddb79e2354b8"] = {
+	"handle": "33849f46cca08a270db4ddb79e2354b8",
+	"widget_name": "Flight Booking",
+	"border_radius": "3",
+	"additional_marker": null,
+	"width": null,
+	"show_logo": false,
+	"show_hotels": false,
+	"form_type": "avia_hotel",
+	"locale": "en",
+	"currency": "inr",
+	"sizes": "default",
+	"search_target": "_self",
+	"active_tab": "avia",
+	"search_host": "travel.kasworld-aero.ml/flights",
+	"hotels_host": "travel.kasworld-aero.ml/hotels",
+	"hotel": "",
+	"hotel_alt": "Hotellook helps you find the best accomodation prices online.",
+	"avia_alt": "",
+	"retargeting": true,
+	"trip_class": "economy",
+	"depart_date": null,
+	"return_date": null,
+	"check_in_date": null,
+	"check_out_date": null,
+	"no_track": false,
+	"powered_by": false,
+	"source_id": null,
+	"id": 244014,
+	"marker": 172519,
+	"origin": {
+		"name": ""
+	},
+	"destination": {
+		"name": ""
+	},
+	"color_scheme": {
+		"name": "white_blue",
+		"icons": "icons_blue",
+		"background": "#ffffff",
+		"color": "#000000",
+		"border_color": "#c1c1c1",
+		"button": "#1b9ed9",
+		"button_text_color": "#ffffff",
+		"input_border": "#ffffff"
+	},
+	"hotels_type": "hotellook_host",
+	"best_offer": {
+		"locale": "en",
+		"currency": "inr",
+		"marker": 172519,
+		"search_host": "travel.kasworld-aero.ml/flights",
+		"offers_switch": false,
+		"api_url": "//minprices-jetradar.aviasales.ru/minimal_prices/offers.json",
+		"routes": []
+	},
+	"hotel_logo_host": null,
+	"search_logo_host": "jetradar.com",
+	"hotel_marker_format": null,
+	"hotelscombined_marker": null,
+	"sources": [],
+	"responsive": true,
+	"height": 460
+};
+
+
+(function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = '//www.travelpayouts.com/widgets/33849f46cca08a270db4ddb79e2354b8.js?v=2135';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
