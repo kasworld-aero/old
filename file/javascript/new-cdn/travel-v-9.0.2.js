da4e29de6fa3f5ae6d1fdeac262b9c2d74e53b15
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-07-11 01:17:08 +0530

    Update travel-v-9.0.2.js

--- file/javascript/new-cdn/travel-v-9.0.2.js
+++ file/javascript/new-cdn/travel-v-9.0.2.js
@@ -1,6 +1,13 @@
-
+document.write('<div class="kas-widget">');
 document.write('<script charset="utf-8" src="https://www.travelpayouts.com/widgets/33849f46cca08a270db4ddb79e2354b8.js?v=2135" async></scr'+'ipt>');
 document.write('<script src="https://kasworld-aero.github.io/old/file/javascript/new-cdn/booking.js"></scr'+'ipt>');
 
+document.write('</div><div class="kas-power"><a href="https://www-golink.blogspot.com/2021/05/free-widget-for-flight-and-hotel-booking.html" target="_blank" style="font-size:7pt;text-align:left;">Get This Widget</a><span style="float:right;"><a href="https://kasworld-aero.blogspot.com" target="_blank" style="font-size:7pt;text-align: right;"> Powered by KASWORLD.</a></span></div>');
+               
+               
+  $('div.kas-power').hide();
+    $('div.kas-widget').bind('mouseover', function() {
+        $('div.kas-power').fadeIn();       
+    });
 
   
