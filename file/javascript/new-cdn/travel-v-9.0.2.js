e24b45fd0872f27d7b09aa5d2c52ed3efa2dd415
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-07-11 00:54:32 +0530

    Update travel-v-9.0.2.js

--- file/javascript/new-cdn/travel-v-9.0.2.js
+++ file/javascript/new-cdn/travel-v-9.0.2.js
@@ -1,6 +1,6 @@
 
-document.write('<script charset="utf-8"src="https://www.travelpayouts.com/widgets/33849f46cca08a270db4ddb79e2354b8.js?v=2135"async></scr'+'ipt>');
-document.write('<script charset="utf-8"src="https://kasworld-aero.github.io/old/file/javascript/new-cdn/booking.js"async></scr'+'ipt>');
+document.write('<script charset="utf-8" src="https://www.travelpayouts.com/widgets/33849f46cca08a270db4ddb79e2354b8.js?v=2135" async></scr'+'ipt>');
+document.write('<script charset="utf-8" src="https://kasworld-aero.github.io/old/file/javascript/new-cdn/booking.js" async></scr'+'ipt>');
 
 
   
