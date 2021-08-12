50f7b108abe2e93fd2c5b3fe89a14f6cedebc104
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-08-12 23:19:49 +0530

    Update fire.js

--- file/javascript/cdn/fire.js
+++ file/javascript/cdn/fire.js
@@ -1,1 +1,4 @@
+var fpost = "<script src='https://kasworld-aero.blogspot.com/feeds/posts/default/-/AIRBUS?alt=json-in-script&max-results=4&callback=mbtlist' rel='preload'></scr"+"ipt>";
+
+  document.getElementById("futurepost").innerHTML = fpost;
 
