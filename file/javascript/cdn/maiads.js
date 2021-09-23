c9dd120d6cc42dfa6826a50ed16778005d5fdc80
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-09-23 21:51:29 +0530

    Update maiads.js

--- file/javascript/cdn/maiads.js
+++ file/javascript/cdn/maiads.js
@@ -4,10 +4,11 @@ let scripts = [
 				'script2.js',
 				'script3.js'
 			];
-
+   window.onload = function(){
 scripts.forEach(function(url) {
 	let mainscript = document.createElement('script');
 	mainscript.src = url;
 	mainscript.async = false;
 	document.body.appendChild(mainscript);
 });
+   }
