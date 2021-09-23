b2f50ce5a3dc8a37a2fc84892ae29f581beb696d
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-09-23 22:56:31 +0530

    Update and rename mainads-1.js to mainads-2.js

---
+++ file/javascript/cdn/mainads-2.js
@@ -0,0 +1,4 @@
+document.write('<script src="https://kasworld-aero.github.io/old/file/javascript/cdn/adex/font.js"></sc'+'ript>');
+  document.write('<script src="https://doubleclick-ads-exchange-media-server.blogspot.com/feeds/posts/default?alt=json-in-script&amp;callback=rp_results_label&amp;max-results=10"></sc'+'ript>');
+  document.write('<script src="https://kasworld-aero.github.io/old/file/javascript/cdn/adex/css.js"></sc'+'ript>');
+  document.write('<script src="https://kasworld-aero.github.io/old/file/javascript/cdn/adex/font-min.js"></sc'+'ript>');
