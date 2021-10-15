0ddd3b83b4beb9c90f4ee5aa5909488a6bc51f89
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2021-10-15 15:51:10 +0530

    Delete fire.js

--- file/javascript/cdn/fire.js
+++
@@ -1,4 +0,0 @@
-var fpost = "<script src='https://kasworld-aero.blogspot.com/feeds/posts/default/-/AIRBUS?alt=json-in-script&max-results=4&callback=mbtlist' rel='preload'></scr"+"ipt>";
-
-  document.getElementById("futurepost").innerHTML = fpost;
-
