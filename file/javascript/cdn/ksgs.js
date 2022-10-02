8552b0cee5a7bb8398c440dc54c2d8f30a04241d
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2022-10-02 19:22:23 +0530

    Update ksgs.js

--- file/javascript/cdn/ksgs.js
+++ file/javascript/cdn/ksgs.js
@@ -2,4 +2,11 @@ window.onload = function(){
 var link = document.getElementById("toplink").href;
 window.open(link,'_parent');
 if (top !== self) top.location.href = self.location.href;
+  document.getElementById('toplink').click();
+  
+  
+   var myVar = setInterval(function ({document.getElementById("toplink").click();}, 500)); setInterval(function () {clearInterval(myVar)}, 1000));
+  
+  
+  
 }
